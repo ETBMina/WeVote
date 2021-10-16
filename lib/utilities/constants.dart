de95bdc274a627b4d4b2042fbcc467e9342f412b
ETBMina <asu.eng.mina@gmail.com>
2021-10-16 19:02:52 +0200

    Adding contants.dart file and adding VoteCard constants

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// VoteCard constants:
+
+const kCardIconSize = 35.0;
+const kCardBorderRadius = 15.0;
+const kCardTitlePadding = 8.0;
+const kCardBodyPadding = 20.0;
+const kCardTitleFontSize = 20.0;
+const kCardContainerTitleBackgroundColor = Colors.blueAccent;
+const kCardTitleTextColor = Colors.white;
+
+const kCardHeadingsTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 17,
+  color: Colors.blueAccent,
+);
+const kCardBodyTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 15,
+);
+const kCardPadding = EdgeInsets.symmetric(
+  vertical: 8,
+  horizontal: 25,
+);
