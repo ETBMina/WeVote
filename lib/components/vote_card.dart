3f33a8f5bb82e1960e1702a4d6bb3bbc27133e78
ETBMina <asu.eng.mina@gmail.com>
2021-11-04 09:44:45 +0200

    - Removing test data - Fixing some bugs

--- lib/components/vote_card.dart
+++ lib/components/vote_card.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:wevote/models/vote.dart';
 import 'package:intl/intl.dart';
 import 'package:wevote/utilities/constants.dart';
 
-// This is a test data
-var vote = Vote(
-  isPublic: true,
-  isSecretVote: false,
-  title:
-      'Who is d the best playerrrrrrrrr rrrrrrr rrrrrr rrrrr rrrrrr rrr rrrrrrr?',
-  description:
-      'This vote is about who is the best football player in the world',
-  choices: {
-    'Messi': 1,
-    'Ronaldo': 1,
-    'Neymar': 1,
-  },
-  createdByEmail: 'mina@gmail.com',
-  isAddingChoicesAllowed: false,
-  noOfChoicesToSelect: 1,
-  isOrderMaters: false,
-  createdDateTime: DateTime.now(),
-  rankingWeights: [],
-  expirationDateTime: DateTime.now().add(Duration(days: 3)),
-);
-
 class VoteCard extends StatelessWidget {
+  final String title;
+  final DateTime expirationDateTime;
+  final String createdByEmail;
+  final VoidCallback onCardPressedCallback;
+
+  VoteCard(
+      {required this.title,
+      required this.expirationDateTime,
+      required this.createdByEmail,
+      required this.onCardPressedCallback});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -37,9 +25,7 @@ class VoteCard extends StatelessWidget {
         clipBehavior: Clip.antiAlias,
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
-          onTap: () {
-            print('Card tapped.');
-          },
+          onTap: onCardPressedCallback,
           child: Column(
             children: [
               Container(
@@ -48,7 +34,7 @@ class VoteCard extends StatelessWidget {
                   padding: const EdgeInsets.all(kCardTitlePadding),
                   child: Center(
                     child: Text(
-                      vote.title,
+                      title,
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
@@ -80,7 +66,7 @@ class VoteCard extends StatelessWidget {
                                   ),
                                   Expanded(
                                     child: Text(
-                                      '${vote.createdByEmail}',
+                                      '${createdByEmail}',
                                       maxLines: 1,
                                       overflow: TextOverflow.ellipsis,
                                       style: kCardBodyTextStyle,
@@ -97,7 +83,7 @@ class VoteCard extends StatelessWidget {
                                     textAlign: TextAlign.left,
                                   ),
                                   Text(
-                                    '${DateFormat("dd-MM-yyyy H:m").format(vote.expirationDateTime)}',
+                                    '${DateFormat("dd-MM-yyyy H:m").format(expirationDateTime)}',
                                     style: kCardBodyTextStyle,
                                     textAlign: TextAlign.left,
                                   ),
